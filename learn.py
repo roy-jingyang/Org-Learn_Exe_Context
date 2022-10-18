82d80b1a92ee1d7aabf19fc3d4b475b148604960
roy-jingyang <7447643+roy-jingyang@users.noreply.github.com>
2022-10-18 18:59:19 +1000

    Update main file

--- learn.py
+++ learn.py
@@ -6,7 +6,6 @@ from copy import deepcopy
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import KFold
 
 from ordinor.io import read_disco_csv
 from ordinor.execution_context import ODTMiner
@@ -16,41 +15,19 @@ fn_log = sys.argv[1]
 
 el = read_disco_csv(fn_log)
 
-# TODO: use only data from selected municipality
-el = el[el['r:municipality'] == 'muni-5']
-
-print(len(pd.unique(el['org:resource'])))
-print(len(pd.unique(el['concept:name'])))
-
-
 # specification
 
-all_cand_attrs = [
-    # WABO
-    #{'attr': 'ct:channel', 'attr_type': 'categorical', 'attr_dim': 'CT'},
-    #{'attr': 'tt:weekday', 'attr_type': 'categorical', 'attr_dim': 'TT'}, 
-    #{'attr': 'tt:ampm', 'attr_type': 'categorical', 'attr_dim': 'TT'},
-    
-    # BPIC15
-    {'attr': 'ct:permit_type', 'attr_type': 'categorical', 'attr_dim': 'CT'},
-    {'attr': 'at:phase', 'attr_type': 'categorical', 'attr_dim': 'AT'},
-    {'attr': 'tt:weekday', 'attr_type': 'categorical', 'attr_dim': 'TT'}, 
-    {'attr': 'tt:ampm', 'attr_type': 'categorical', 'attr_dim': 'TT'},
-    
-    # BPIC17
-    #{'attr': 'ct:loan_goal', 'attr_type': 'categorical', 'attr_dim': 'CT'},
-    #{'attr': 'ct:application_type', 'attr_type': 'categorical', 'attr_dim': 'CT'},
-    #{'attr': 'ct:requested_amount', 'attr_type': 'numeric', 'attr_dim': 'CT'},
-    #{'attr': 'at:event_origin', 'attr_type': 'categorical', 'attr_dim': 'AT'},
-    #{'attr': 'tt:weekday', 'attr_type': 'categorical', 'attr_dim': 'TT'}, 
-    #{'attr': 'tt:ampm', 'attr_type': 'categorical', 'attr_dim': 'TT'},
-]
-
-spec = dict()
-spec['cand_attrs'] = all_cand_attrs
-
+spec = {
+    'type_def_attrs': {
+        # BPIC15
+        'ct:permit_type': {'attr_type': 'categorical', 'attr_dim': 'CT'},
+        'at:phase': {'attr_type': 'categorical', 'attr_dim': 'AT'},
+        'tt:weekday': {'attr_type': 'categorical', 'attr_dim': 'TT'}, 
+        'tt:ampm': {'attr_type': 'categorical', 'attr_dim': 'TT'},
+    }
+}
 
 # run algorithm
 
-miner = ODTMiner(el, spec, eps=1e-2, trace_history=True)
+miner = ODTMiner(el, spec, max_height=12, trace_history=True)
 
